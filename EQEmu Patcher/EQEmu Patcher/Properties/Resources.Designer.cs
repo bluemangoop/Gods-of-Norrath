c973ce1762c833fd429bf6328e3431e2bf668d7b
bluemangoop <bluemangoop@users.noreply.github.com>
2026-06-12 06:49:29 +0000

    v1.0.0.18: replace splash image + icon with Coirnav / Innoruuk

--- EQEmu Patcher/EQEmu Patcher/Properties/Resources.Designer.cs
+++ EQEmu Patcher/EQEmu Patcher/Properties/Resources.Designer.cs
@@ -79,7 +79,14 @@ namespace EQEmu_Patcher.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
+        internal static System.Drawing.Bitmap coirnav1 {
+            get {
+                object obj = ResourceManager.GetObject("coirnav1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
